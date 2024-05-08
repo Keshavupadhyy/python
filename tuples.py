201a0f2011fc1818356256e7a9e96309db91af69
Keshav Upadhyay <109733099+Keshavupadhyy@users.noreply.github.com>
2024-05-08 20:47:35 +0530

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,14 @@
+"""
+In Python, a tuple is a collection of ordered and immutable elements.
+Once a tuple is created, its elements cannot be changed,
+"""
+my_tuple = (1,2,3,4,5)
+# print(my_tuple[1])
+# print(5 in my_tuple)
+# new_tuple = my_tuple[1:4]
+# print(new_tuple)
+
+#methods in tuple are only two
+
+print(my_tuple.index(5))
+print(my_tuple.count(5))
